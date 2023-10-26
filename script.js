e9a9a2cdcb0ee0c005814c1e0405848877bab085
Jason Yang <jyang6254@gmail.com>
2023-10-26 12:47:40 -0700

    js stuff

--- script.js
+++ script.js
@@ -8,21 +8,26 @@ function createBoard(){
             let pawn = document.createElement('div');
             if(j < 3){
                 pawn.classList.add('pawn');
-                pawn.style.backgroundColor = "#222";
+                pawn.classList.add('red');
             }
             if(j > 4){
                 pawn.classList.add('pawn');
-                pawn.style.backgroundColor = "#d00";
+                pawn.classList.add('black');
             }
             if(i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1){
                 square.classList.add('light');
             } else {
                 square.classList.add('dark');
                 square.appendChild(pawn);
-                pawn.addEventListener("click", function(){
+                square.classList.toggle('occupied');
+                pawn.addEventListener("mouseover", function(){
                     showMoves(pawn);
                 });
+                pawn.addEventListener("mouseout", function(){
+                    hideMoves(pawn);
+                });
             }
+            square.id = j + ',' + i;
             boardContainer.appendChild(square);
         }
     }
@@ -30,21 +35,52 @@ function createBoard(){
 
 createBoard();
 function showMoves(pawn){
-
-}
-
-class square{
-    constructor(color){
-        this.color = color;
-        this.hasPawn = false;
+    let pawnPos = (pawn.parentElement.id);
+    let pawnColor = pawn.className[5];
+    // console.log(pawn.parentElement.classList);
+    // console.log(pawnPos);
+    // console.log(pawnColor); //(pawnPos[0]-0) + 1;
+    if(pawnColor == 'r'){
+        let newX = (pawnPos[0]-0) + 1;
+        let newY1 = (pawnPos[2]-0) + 1;
+        let newY2 = (pawnPos[2]-0) - 1;
+        
+        let pos1 = (newX + ',' + newY1);
+        let move1 = document.getElementById(pos1);
+        let pos2 = (newX + ',' + newY2);
+        let move2 = document.getElementById(pos2);
+        move1.style.backgroundColor = '#6c2525';
+        move2.style.backgroundColor = '#6c2525';
     }
-}
-class pawn{
-    constructor(color){
-        this.color = color;
-        this.isKing = false;
+    else if(pawnColor =='b'){
+
     }
+    
 }
+function hideMoves(pawn){
+    let pawnPos = (pawn.parentElement.id);
+    let pawnColor = pawn.className[5];
+    // console.log(pawn.parentElement.classList);
+    // console.log(pawnPos);
+    // console.log(pawnColor);
+    if(pawnColor == 'r'){
+        let newX = (pawnPos[0]-0) + 1;
+        let newY1 = (pawnPos[2]-0) + 1;
+        let newY2 = (pawnPos[2]-0) - 1;
+        
+        let pos1 = (newX + ',' + newY1);
+        let move1 = document.getElementById(pos1);
+        let pos2 = (newX + ',' + newY2);
+        let move2 = document.getElementById(pos2);
+        move1.style.backgroundColor = '#4a0303';
+        move2.style.backgroundColor = '#4a0303';
+    }
+    else if(pawnColor =='b'){
+
+    }
+    
+}
+
 class player{
     constructor(color){
         this.color = color;
